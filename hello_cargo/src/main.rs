3bddc6dc175aff43a4fb48eb854fb1d0b14f9bac
Christian Wander <c.wander@synoa.de>
2020-08-29 13:31:00 +0200

    added looping and handling of shitty input

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -8,24 +8,36 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("The secret number is {}", secret_number);
 
-    println!("Plz input your guess.");
-    let mut guess = String::new();
+    loop {
+        println!("Plz input your guess.");
+        
+        let mut guess = String::new();
+        let bytes_read = io::stdin()
+            .read_line(&mut guess) // read_line returns an io::Result, which is an enum.
+            .expect("Failed to read line"); //  It can be "Ok" or "Err".
+            // if it's an "Ok", the expect function returns the no of bytes read.
+            // if it's an "Err", the expect function will crash the program.
 
-    let bytes_read = io::stdin()
-        .read_line(&mut guess) // read_line returns an io::Result, which is an enum.
-        .expect("Failed to read line"); //  It can be "Ok" or "Err".
-        // if it's an "Ok", the expect function returns the no of bytes read.
-        // if it's an "Err", the expect function will crash the program.
-
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-
-    println!("Bytes read: {}", bytes_read);
-    println!("You guessed: {}", guess);
-
-    match guess.cmp(&secret_number) {
-        // the lines here are "arms"
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("Epic Win!"),
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Enter a number, idiot!");
+                continue;
+            }
+        };
+    
+        println!("Bytes read: {}", bytes_read);
+        println!("You guessed: {}", guess);
+    
+        match guess.cmp(&secret_number) {
+            // the lines here are "arms"
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("Epic Win!");
+                break;
+            }
+        }
     }
+
 }
