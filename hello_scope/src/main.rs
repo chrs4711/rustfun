fd43ae99b99bc23786195ce85d14334ff75efb48
Christian Wander <c.wander@synoa.de>
2020-08-30 17:11:09 +0200

    some cleanup

--- hello_scope/src/main.rs
+++ hello_scope/src/main.rs
@@ -2,18 +2,30 @@ fn main() {
 
     println!("Hello, scope!");
     nothing_special(123);
+    super_normal_number_stuff();
     
     string_fun();
 
     more_string_fun();
-
-    super_normal_number_stuff();
 }
 
 /// nothing special here
 fn nothing_special(x: i32)  {
     println!("value: {}", x);
 }
+
+/// now we have _two_ variables of whatever the type of '5' is on the stack.
+fn super_normal_number_stuff() {
+    let mut x = 5;
+    let y = x;
+
+    println!("x: {}, y: {}", x, y);
+
+    // no references, just values.
+    x = 2;
+    println!("x: {}, y: {}", x, y);
+}
+
 
 fn string_fun() {
     
@@ -46,17 +58,4 @@ fn more_string_fun() {
 
 fn print_string_length(s: &String) {
     println!("Lengh of '{}': {} bytes", s, s.len());
-}
-
-
-/// now we have _two_ variables of whatever the type of '5' is on the stack.
-fn super_normal_number_stuff() {
-    let mut x = 5;
-    let y = x;
-
-    println!("x: {}, y: {}", x, y);
-
-    // no references, just values.
-    x = 2;
-    println!("x: {}, y: {}", x, y);
 }
